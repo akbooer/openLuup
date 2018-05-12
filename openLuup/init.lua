84abece40be95f997daf40e973e522bc9bd12a10
rafale77 <anhmantran@yahoo.com>
2018-05-11 22:45:24 -0700

    Update init.lua

--- openLuup/init.lua
+++ openLuup/init.lua
@@ -1,6 +1,6 @@
 local ABOUT = {
   NAME          = "openLuup.init",
-  VERSION       = "2018.04.29",
+  VERSION       = "2018.05.11",
   DESCRIPTION   = "initialize Luup engine with user_data, run startup code, start scheduler",
   AUTHOR        = "@akbooer",
   COPYRIGHT     = "(c) 2013-2018 AKBooer",
@@ -48,6 +48,7 @@ local ABOUT = {
 -- 2018.04.04  add POP3 server
 -- 2018.04.23  re-order module loading (to tidy startup log banners)
 -- 2018.04.25  change server module name back to http, and use opeLuup.HTTP... attributes
+-- 2018.05.11  add category and subcategory
 
 
 local logs = require "openLuup.logs"
