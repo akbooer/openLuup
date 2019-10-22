66b65a3637380668333595734d19c9e263ba89d6
akbooer <ak_booer@ieee.org>
2019-10-22 17:42:08 +0100

    2019.10.22b
    - fix VeraBridge version number

--- openLuup/L_VeraBridge.lua
+++ openLuup/L_VeraBridge.lua
@@ -1,6 +1,6 @@
 ABOUT = {
   NAME          = "VeraBridge",
-  VERSION       = "2019.05.11",
+  VERSION       = "2019.10.22",
   DESCRIPTION   = "VeraBridge plugin for openLuup",
   AUTHOR        = "@akbooer",
   COPYRIGHT     = "(c) 2013-2019 AKBooer",
