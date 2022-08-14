99fe410ad3e4ec724dae16bcafb8d198fa487518
akbooer <ak_booer@ieee.org>
2022-08-14 13:29:09 +0100

    2022.08.14
    - fix error message for unknown HTTP request type
    - thanks @Donato

--- openLuup/L_openLuup.lua
+++ openLuup/L_openLuup.lua
@@ -1,6 +1,6 @@
 ABOUT = {
   NAME          = "L_openLuup",
-  VERSION       = "2022.07.31c",
+  VERSION       = "2022.08.14",
   DESCRIPTION   = "openLuup device plugin for openLuup!!",
   AUTHOR        = "@akbooer",
   COPYRIGHT     = "(c) 2013-2022 AKBooer",
