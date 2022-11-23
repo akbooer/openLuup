2ce9f3eaa2f0f5b0ba74433db72bd6d6a61359ac
akbooer <ak_booer@ieee.org>
2022-11-23 21:24:39 +0000

    2022.11.22
    - 2022 Master update

--- openLuup/L_openLuup.lua
+++ openLuup/L_openLuup.lua
@@ -1,6 +1,6 @@
 ABOUT = {
   NAME          = "L_openLuup",
-  VERSION       = "2022.11.15c",
+  VERSION       = "2022.11.22",
   DESCRIPTION   = "openLuup device plugin for openLuup!!",
   AUTHOR        = "@akbooer",
   COPYRIGHT     = "(c) 2013-2022 AKBooer",
